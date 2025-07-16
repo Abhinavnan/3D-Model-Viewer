4e330b23d8c1753804563a0bfd23e5541a68f508
Mustilago <mustilagonam@gmail.com>
2025-07-17 00:42:42 +0530

    lazy loading

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,23 +1,27 @@
-import React from 'react'
+import React, { Suspense, lazy } from 'react';
+import {GridLoader} from "react-spinners";
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Header from './Components/Header';
-import Upload from './Components/content/Upload';
-import ViewModel from './Components/content/ViewModel';
-import View from './Components/content/View';
+const Upload = lazy(() => import('./Components/content/Upload'));
+const Header = lazy(() => import('./Components/Header'));
+const ViewModel = lazy(() => import('./Components/content/ViewModel'));
+const View = lazy(() => import('./Components/content/View'));
 import './App.css';
 
 function App() {
-
+  const loading = () => <GridLoader color="#6d6b6bff" 
+        cssOverride={{position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)'}} />
   return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Upload />} />
-        <Route path="/view-model/:modelId" element={<ViewModel />} />
-        <Route path="/view" element={<View />} />
-        <Route path='*' element={<Upload />} />
-      </Routes>
-    </Router>
+    <Suspense fallback={loading()}>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Upload />} />
+          <Route path="/view-model/:modelId" element={<ViewModel />} />
+          <Route path="/view" element={<View />} />
+          <Route path='*' element={<Upload />} />
+        </Routes>
+      </Router>
+    </Suspense>
   )
 }
 
